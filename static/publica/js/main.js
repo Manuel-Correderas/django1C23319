75f6c8d382a459028cd9ab863d9f1fd1d96898b7
Manuel-Correderas <112085585+Manuel-Correderas@users.noreply.github.com>
2023-05-22 20:46:52 -0300

    Add files via upload

--- static/publica/js/main.js
+++ static/publica/js/main.js
@@ -1,29 +1,27 @@
-const { createApp } = Vue  //creo un objeto VUE llamdo createApp
-
-  createApp({
-    data() {
-      return {
-       
-        url:'https://manucorre88.pythonanywhere.com/productos',
-        productos:[]
-      } 
-    },   
-    methods: {
-      fetchData(url) {
-
-          fetch(url)
-              .then(response => response.json())
-              .then(data => { 
-                  this.productos=data
-                  console.log(this.productos)
-              })
-
-      }
-    },
-    created(){
-
-      this.fetchData(this.url) 
+const { createApp } = Vue;
+  createApp( {data() {
+    return { 
+ 
+    url: 'https://manucorre88.pythonanywhere.com/productos',
+    productos: [], // Inicializa la propiedad 'productos' como un arreglo vacío
     }
+  },
+  methods: {
+    fetchData(url) {
 
-    
-  }).mount('#app')
+        fetch(url)
+            .then(response => response.json())
+            .then(data => { 
+                this.productos=data
+                console.log((this.productos))
+            })
+
+    }
+  },
+  created(){
+
+    this.fetchData(this.url) 
+  }
+
+  
+}).mount('#app')
